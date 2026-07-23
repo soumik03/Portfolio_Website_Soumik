a3478f53ee9f547ec62adf0a61086d4b498ab06c
soumik03 <soumik.datta2011@gmail.com>
2026-07-23 13:36:13 +0530

    Day_9_changes

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 console.log("JavaScript is connected! 🚀");
 
 // 2. Display the current year in footer
-let footerYear=document.querySelector('.footer-year');
+const footerYear=document.querySelector('.footer-year');
+console.log(footerYear);
 if(footerYear){
     footerYear.textContent=new Date().getFullYear();;
 }
@@ -12,12 +13,55 @@ if(footerYear){
 //3. Greeting based on time of day
 function getGreeting(){
     const hour =new Date().getHours();
+    console.log(hour);
     if (hour < 12) return "Good Morning";
     if (hour < 17) return "Good Afternoon";
     return "Good Evening"
 }
 
 const heroTitle = document.querySelector('.hero-section h1');
+console.log(heroTitle);
 if (heroTitle){
     heroTitle.textContent = `${getGreeting()}, I'm Soumik Datta 👋`;
 }
+
+// ==== MOBILE MENU TOGGLE ===
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () =>{
+    navLinks.classList.toggle('open');
+    menuToggle.setAttribute('aria-expanded',navLinks.classList.contains('open'));
+});
+
+// === SCROLL-BASED NAVBAR STYLING ====
+const header = document.querySelector('.site-header');
+
+window.addEventListener('scroll', () =>{
+    if (window.scrollY > 50){
+        header.classList.add('scrolled')
+    }
+    else{
+        header.classList.remove('scrolled')
+    }
+})
+
+// === ACTIVE NAV LINK on scroll ====
+const sections = document.querySelectorAll('section[id]');
+const navItems = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () =>{
+    let current ='';
+    sections.forEach(section =>{
+        if(window.scrollY >= section.offsetTop-100){
+            current=section.getAttribute('id');
+        }
+    })
+    navItems.forEach(link =>{
+        link.classList.remove('active');
+        if(link.getAttribute('href') === `${current}`) {
+            link.classList.add('active');
+        }
+    })
+})
